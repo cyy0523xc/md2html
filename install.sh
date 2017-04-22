1c2e59fac57d93402f09239fe760fdde1f70f7ff
alex cai <caiyingyao@ibbd.net>
2017-04-22 23:41:25 +0800

    md2html

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# 
+# 安装脚本
+# Author: Alex
+# Created Time: 2017年04月22日 星期六 22时32分31秒
+
+if [ ! -f ./md2html ]; then 
+    echo "请在当前目录下运行按照文件"
+    exit 1
+fi
+
+#npm install remarkable -g
+
+sed -i "s#^source_root=\$#source_root="$PWD"#" ./md2html
+ln -s "$PWD/md2html" /usr/bin/md2html
